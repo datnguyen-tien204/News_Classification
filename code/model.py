9c0a6c7f1f286bb5ee1d9134cabdb31e3761e559
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2023-11-27 19:50:07 +0700

    Update model.py

--- code/model.py
+++ code/model.py
@@ -1,47 +1,47 @@
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from torch.autograd import Variable
+# import torch
+# import torch.nn as nn
+# from torch.nn import functional as F
+# from torch.autograd import Variable
 
-class AttentionModel(torch.nn.Module):
-    def __init__(self, output_size, hidden_size, vocab_size, embedding_length):
-        super(AttentionModel, self).__init__()
-        self.output_size = output_size
-        self.hidden_size = hidden_size
-        self.vocab_size = vocab_size
-        self.embedding_length = embedding_length
-        self.batch_size = 0  # Bỏ đi biến batch_size từ đây
+# class AttentionModel(torch.nn.Module):
+#     def __init__(self, output_size, hidden_size, vocab_size, embedding_length):
+#         super(AttentionModel, self).__init__()
+#         self.output_size = output_size
+#         self.hidden_size = hidden_size
+#         self.vocab_size = vocab_size
+#         self.embedding_length = embedding_length
+#         self.batch_size = 0  # Bỏ đi biến batch_size từ đây
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
-        self.lstm = nn.LSTM(embedding_length, hidden_size)
-        self.label = nn.Linear(hidden_size, output_size)
+#         self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
+#         self.lstm = nn.LSTM(embedding_length, hidden_size)
+#         self.label = nn.Linear(hidden_size, output_size)
 
-        # Xác định thiết bị cho mô hình
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.to(self.device)
+#         # Xác định thiết bị cho mô hình
+#         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#         self.to(self.device)
 
-    def attention_net(self, lstm_output, final_state):
-        hidden = final_state.squeeze(0)
-        attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
-        soft_attn_weights = F.softmax(attn_weights, 1)
-        new_hidden_state = torch.bmm(
-            lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)
-        ).squeeze(2)
+#     def attention_net(self, lstm_output, final_state):
+#         hidden = final_state.squeeze(0)
+#         attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
+#         soft_attn_weights = F.softmax(attn_weights, 1)
+#         new_hidden_state = torch.bmm(
+#             lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)
+#         ).squeeze(2)
 
-        return new_hidden_state
+#         return new_hidden_state
 
-    def forward(self, input_sentences):
-        input = self.word_embeddings(input_sentences)
-        input = input.permute(1, 0, 2)
+#     def forward(self, input_sentences):
+#         input = self.word_embeddings(input_sentences)
+#         input = input.permute(1, 0, 2)
 
-        batch_size = input.shape[1]  # Xác định batch_size dựa trên kích thước đầu vào
-        h_0 = torch.zeros(1, batch_size, self.hidden_size).to(self.device)
-        c_0 = torch.zeros(1, batch_size, self.hidden_size).to(self.device)
+#         batch_size = input.shape[1]  # Xác định batch_size dựa trên kích thước đầu vào
+#         h_0 = torch.zeros(1, batch_size, self.hidden_size).to(self.device)
+#         c_0 = torch.zeros(1, batch_size, self.hidden_size).to(self.device)
 
-        output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
-        output = output.permute(1, 0, 2)
+#         output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+#         output = output.permute(1, 0, 2)
 
-        attn_output = self.attention_net(output, final_hidden_state)
-        logits = self.label(attn_output)
+#         attn_output = self.attention_net(output, final_hidden_state)
+#         logits = self.label(attn_output)
 
-        return logits
+#         return logits
